063eb78837ef92e166d7e8bc878727c8bb6253b3
Meteorstrike <k.h.materials@gmail.com>
2024-03-28 17:52:33 +0000

    Upgrading buttons etc

--- src/main.py
+++ src/main.py
@@ -13,10 +13,17 @@ from beltcolour import BeltColour
 from freeform import Freeform
 from data.freeform1 import string
 from movebuilder import MoveBuilder
-
+from kivymd.uix.button import MDTextButton
+from kivymd.uix.label import MDLabel
+from kivymd.uix.dropdownitem import dropdownitem
 
 
 class HomeWindow(Screen):
+    # about = ObjectProperty(None)
+    # disclaimer = ObjectProperty(None)
+    # def about_screen(self):
+    #
+    #     return
     pass
 
 
@@ -28,22 +35,15 @@ class RefWindow(Screen):
     pass
 
 
-# class Freeform(Screen):
-#     pass
-
-
 class PracticeWindow(Screen):
     pass
 
-
-# class WindowManager(ScreenManager):
-#     FadeTransition()
 
 class WindowManager(ScreenManager):
     pass
 
 
-# kv = Builder.load_file("main.kv")
+kv = Builder.load_file("main.kv")
 
 # sm = WindowManager()
 #
@@ -59,29 +59,29 @@ class WindowManager(ScreenManager):
 class MyMainApp(MDApp):
 
     def build(self):
-        # self.sm = WindowManager(transition=FadeTransition())
-        # self.sm = WindowManager()
-        #
-        # self.sm.add_widget(HomeWindow(name="home"))
-        # self.sm.add_widget(StartWindow(name="startpage"))
-        # self.sm.add_widget(RefWindow(name="reference"))
-        # self.sm.add_widget(MoveSearch(name="movesearch"))
-        # self.sm.add_widget(BeltColour(name="beltcolour"))
-        # self.sm.add_widget(Freeform(name="freeform"))
-        # self.sm.add_widget(PracticeWindow(name="practice"))
-        # self.sm.add_widget(MoveBuilder(name="movebuilder"))
-        # self.sm.current = "home"
+        self.sm = WindowManager()
+
+        self.sm.add_widget(HomeWindow(name="home"))
+        self.sm.add_widget(StartWindow(name="startpage"))
+        self.sm.add_widget(RefWindow(name="reference"))
+        self.sm.add_widget(MoveSearch(name="movesearch"))
+        self.sm.add_widget(BeltColour(name="beltcolour"))
+        self.sm.add_widget(Freeform(name="freeform"))
+        self.sm.add_widget(PracticeWindow(name="practice"))
+        self.sm.add_widget(MoveBuilder(name="movebuilder"))
+        self.sm.current = "home"
 
         self.theme_cls.theme_style = "Dark"
 
-        self.theme_cls.primary_palette = "Teal"
-        self.theme_cls.accent_palette = "Red"
-        # return self.sm
+        self.theme_cls.primary_palette = "Red"
+        self.theme_cls.accent_palette = "Blue"
+        return self.sm
 
-        return Builder.load_file("main.kv")
+        # return Builder.load_file("main.kv")
 
 
 if __name__ == "__main__":
     MyMainApp().run()
 
 
+# TODO: - About, - Disclaimer
